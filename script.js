c6b712b477d9bc13e9aecff7158dec8210611695
Isabelle G. Jesuino <isabellegjesuino@gmail.com>
2024-08-05 21:21:58 -0300

    created smooth scroll and animation

--- script.js
+++ script.js
@@ -45,3 +45,58 @@ function initAccordion() {
 }
 
 initAccordion();
+
+// Scroll Suave Link Interno
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+
+    // Forma alternativa
+    //   const topo = section.offsetTop;
+    //   window.scrollTo({
+    //     top: topo,
+    //     behavior: "smooth",
+    //   });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+
+initScrollSuave();
+
+// Scroll e Animação
+
+function initAnimationScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        if (isSectionVisible) {
+          section.classList.add("ativo");
+        }
+      });
+    }
+
+    animaScroll();
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+
+initAnimationScroll();
