6dcb3cf590378fe66e885e24d7cf883685f7f5d3
Isabelle G. Jesuino <isabellegjesuino@gmail.com>
2024-08-08 18:08:17 -0300

    moduled script and created global css

---
+++ js/modules/animation-scroll.js
@@ -0,0 +1,22 @@
+// Scroll e Animação
+
+export default function initAnimationScroll() {
+  const sections = document.querySelectorAll('[data-anime="scroll"]');
+
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        if (isSectionVisible) {
+          section.classList.add("ativo");
+        }
+      });
+    }
+
+    animaScroll();
+    window.addEventListener("scroll", animaScroll);
+  }
+}
---
+++ js/script.js
@@ -0,0 +1,9 @@
+import initSoftScroll from "./modules/soft-scroll.js";
+import initTabNav from "./modules/tab-nav.js";
+import initAccordion from "./modules/accordion.js";
+import initAnimationScroll from "./modules/animation-scroll.js";
+
+initSoftScroll();
+initAnimationScroll();
+initAccordion();
+initTabNav();
