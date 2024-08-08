789d1faa0510aaa8ca479c2b02fcdf40a9cb291e
Isabelle G. Jesuino <isabellegjesuino@gmail.com>
2024-08-08 19:06:49 -0300

    created modal

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initSoftScroll from "./modules/soft-scroll.js";
 import initTabNav from "./modules/tab-nav.js";
 import initAccordion from "./modules/accordion.js";
 import initAnimationScroll from "./modules/animation-scroll.js";
+import initModal from "./modules/modal.js";
 
 initSoftScroll();
 initAnimationScroll();
 initAccordion();
 initTabNav();
+initModal();
