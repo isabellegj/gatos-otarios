bfbac6611d54a462c8e5bca9026692d967c314c0
Isabelle G. Jesuino <isabellegjesuino@gmail.com>
2024-08-08 21:15:37 -0300

    created tooltip

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initTabNav from "./modules/tab-nav.js";
 import initAccordion from "./modules/accordion.js";
 import initAnimationScroll from "./modules/animation-scroll.js";
 import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
 
 initSoftScroll();
 initAnimationScroll();
 initAccordion();
 initTabNav();
 initModal();
+initTooltip();
