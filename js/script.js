7a1a57617a9c3fee42c7466cb207864b5f84dc25
Isabelle G. Jesuino <isabellegjesuino@gmail.com>
2024-08-09 20:39:41 -0300

    created menu-mobile

--- js/script.js
+++ js/script.js
@@ -2,16 +2,14 @@ import initSoftScroll from "./modules/soft-scroll.js";
 import initTabNav from "./modules/tab-nav.js";
 import initAccordion from "./modules/accordion.js";
 import initAnimationScroll from "./modules/animation-scroll.js";
-import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
-import outsideClick from "./modules/outsideclick.js";
+import initMenuMobile from "./modules/menu-mobile.js";
 
 initSoftScroll();
 initAnimationScroll();
 initAccordion();
 initTabNav();
-initModal();
 initTooltip();
 initDropdownMenu();
-outsideClick();
+initMenuMobile();
