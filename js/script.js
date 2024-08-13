ec986bd7bd2da6a81b9a13cd5b574673434c8cc3
Isabelle G. Jesuino <isabellegjesuino@gmail.com>
2024-08-13 18:46:15 -0300

    created fetch-animais

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@ import initAnimationScroll from "./modules/animation-scroll.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
-import initAnimaNumeros from "./modules/anima-numeros.js";
 import initFuncionamento from "./modules/funcionamento.js";
+import initFetchAnimais from "./modules/fetch-animais.js";
 
 initSoftScroll();
 initAnimationScroll();
@@ -15,5 +15,5 @@ initTabNav();
 initTooltip();
 initDropdownMenu();
 initMenuMobile();
-initAnimaNumeros();
 initFuncionamento();
+initFetchAnimais();
