ec9af3cde7627747d0c613a95d9408c34fd89f5a
Isabelle G. Jesuino <isabellegjesuino@gmail.com>
2024-08-20 17:33:43 -0300

    refactor: anima-numeros

--- js/modules/anima-numeros.js
+++ js/modules/anima-numeros.js
@@ -8,7 +8,7 @@ export default function initAnimaNumeros() {
 
       let start = 0;
       const timer = setInterval(() => {
-        start = start + incremento;
+        start += incremento;
         numero.innerText = start;
         if (start > total) {
           numero.innerText = total;
